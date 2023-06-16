d72f63ff90b119ac229bed3f570401a0bccf7b4a
anas <anas4a44@gmail.com>
2023-06-16 02:36:20 +0000

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
