93ee86285d813cfd2a3bf6422ee855ea77cccb5b
anas <anas4a44@gmail.com>
2023-06-19 17:58:46 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,8 @@
  *
  * return: Always 0 (success)
 */
-
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
