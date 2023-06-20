42d347b8b5510837bdaa99af60aaf5eeacee2ca3
anas <anas4a44@gmail.com>
2023-06-19 17:29:10 -0700

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print numbers from 0 to 99.
- * Numbers must be separated by commas followed by a space.
- * You can only use `putchar` to print to console.
+ * Description: Print all possible combinations of two digits.
+ * Numbers must be separated by commas and a space.
+ * 01 and 10 are considered as the same combination of the two digits.
+ * Print only the smallest combination of two digits.
+ * Numbers should be printed in ascending order, with two digits.
+ * You can only use `putchar`.
  * You can only use `putchar` up to 5 times.
- * You are not allowed to use variables of type `char`.
+ * You are not allowed to use any variable of type `char`.
  * Return: 0
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
+	int a, b, c;
 
-	for (a = 0; a < 100; a++)
+	a = 0;
+
+	while (a < 100)
 	{
-		b = a / 10;
-		c = a % 10;
+		b = a % 10; /* singles digit */
+		c = a / 10; /* doubles digit */
 
-		putchar(b + '0');
-		putchar(c + '0');
-
-		if (a < 99)
+		if (c < b)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
+
+		a++;
 	}
 	putchar('\n');
 
