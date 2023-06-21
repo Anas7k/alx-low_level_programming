ad5b4294e3f1ff4114f5ee3cc9ebf1600f44d169
anas <anas4a44@gmail.com>
2023-06-20 18:55:20 -0700

    a

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+}	
